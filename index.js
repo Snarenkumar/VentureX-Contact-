8e8fcb5640a6d2df8b8c85796d5063d8e3742d35
S Naren Kumar <vssnaren@gmail.com>
2025-02-27 23:45:41 +0530

    alomst completed the navbar

---
+++ index.js
@@ -0,0 +1,26 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const nav = document.querySelector(".nav");
+//     const navTrigger = document.querySelector(".navTrigger");
+//     const navList = document.querySelector(".navlinks");
+
+//     // Shrink navbar on scroll
+//     window.addEventListener("scroll", function() {
+//         if (window.scrollY > 50) {
+//             nav.classList.add("affix");
+//         } else {
+//             nav.classList.remove("affix");
+//         }
+//     });
+
+//     // Toggle mobile menu
+//     navTrigger.addEventListener("click", function() {
+//         navTrigger.classList.toggle("active");
+//         nav.classList.toggle("active");
+//     });
+// });
+
+
+
+// const counterval = 0; 
+
+// document.getElementById("counter").innerText =counterval;
