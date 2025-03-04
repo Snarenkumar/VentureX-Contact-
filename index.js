fb6f7276461edb0684028288da5ee90caf9bdb57
S Naren Kumar <vssnaren@gmail.com>
2025-03-04 16:27:44 +0530

    updateing witha newer version of the code , which was completed yesterday

--- index.js
+++ index.js
@@ -24,3 +24,32 @@
 const counterval = 0; 
 
 document.getElementById("counter").innerText =counterval;
+
+const carouselInner = document.querySelector('.carousel-inner');
+const items = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.carousel-control.prev');
+const nextButton = document.querySelector('.carousel-control.next');
+let index = 0;
+
+function updateCarousel() {
+  const offset = -index * 100;
+  carouselInner.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+  } else {
+    index = items.length - 1;
+  }
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  if (index < items.length - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  updateCarousel();
+});
